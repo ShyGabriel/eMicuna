d94faac454c114f1bbc651f0dfc4dec11f1df7e0
ShyGabriel <hegamaja@gmail.com>
2020-10-05 22:27:02 -0500

    cap30 funcion Admin - Ordenes

--- app/src/main/java/com/example/emicuna/AdminNewOrdersActivity.java
+++ app/src/main/java/com/example/emicuna/AdminNewOrdersActivity.java
@@ -1,15 +1,79 @@
 package com.example.emicuna;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.emicuna.Model.AdminOrders;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AdminNewOrdersActivity extends AppCompatActivity {
+    private RecyclerView ordersList;
+    private DatabaseReference ordersRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_new_orders);
+
+        ordersRef = FirebaseDatabase.getInstance().getReference().child("Orders");
+        ordersList = findViewById(R.id.order_list);
+        ordersList.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<AdminOrders> options = new FirebaseRecyclerOptions.Builder<AdminOrders>()
+                .setQuery(ordersRef, AdminOrders.class)
+                .build();
+
+        FirebaseRecyclerAdapter<AdminOrders, AdminOrdersViewHolder> adapter = new FirebaseRecyclerAdapter<AdminOrders, AdminOrdersViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull AdminOrdersViewHolder holder, int i, @NonNull AdminOrders model) {
+                holder.userName.setText(model.getName());
+                holder.userPhoneNumber.setText(model.getPhone());
+                holder.userTotalPrice.setText(model.getTotalAmount());
+                holder.userDataTime.setText(model.getDate()+" - "+ model.getTime());
+                holder.userShippingAddress.setText(model.getAddress()+", "+model.getCity());
+
+            }
+
+            @NonNull
+            @Override
+            public AdminOrdersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.orders_layout, parent, false);
+                return new AdminOrdersViewHolder(view);
+            }
+        };
+        ordersList.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+    public static class AdminOrdersViewHolder extends RecyclerView.ViewHolder{
+        public TextView userName, userPhoneNumber, userTotalPrice, userDataTime, userShippingAddress;
+        public Button showOrderBtn;
+
+        public AdminOrdersViewHolder(@NonNull View itemView) {
+            super(itemView);
+            userName = itemView.findViewById(R.id.order_user_name);
+            userPhoneNumber = itemView.findViewById(R.id.order_phone_number);
+            userTotalPrice = itemView.findViewById(R.id.order_total_price);
+            userDataTime = itemView.findViewById(R.id.order_date_time);
+            userShippingAddress = itemView.findViewById(R.id.order_address_city);
+            showOrderBtn = itemView.findViewById(R.id.show_all_products_btn);
+        }
     }
 }
