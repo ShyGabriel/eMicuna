ad0bd5eded71e73ff8f29ac717a85af6261572be
sssebastian <ubaldosebastian25@gmail.com>
2020-10-06 04:23:22 -0500

    commit parte 32 - Eliminar pedidos (Admin) 😎

--- app/src/main/java/com/example/emicuna/AdminNewOrdersActivity.java
+++ app/src/main/java/com/example/emicuna/AdminNewOrdersActivity.java
@@ -1,10 +1,12 @@
 package com.example.emicuna;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -62,6 +64,33 @@ public class AdminNewOrdersActivity extends AppCompatActivity {
                     }
                 });
 
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        CharSequence options[] = new CharSequence[]{
+                                "Yes",
+                                "No"
+                        };
+                        AlertDialog.Builder builder = new AlertDialog.Builder(AdminNewOrdersActivity.this);
+                        builder.setTitle("¿Has enviado esta orden?");
+
+                        builder.setItems(options, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                                if ( i == 0){
+                                    String uID = getRef(position).getKey();
+                                    RemoveOrder(uID);
+                                }else{
+                                    finish();
+                                }
+                            }
+                        });
+                        builder.show();
+                    }
+                });
+
             }
 
             @NonNull
@@ -74,6 +103,8 @@ public class AdminNewOrdersActivity extends AppCompatActivity {
         ordersList.setAdapter(adapter);
         adapter.startListening();
     }
+
+
 
     public static class AdminOrdersViewHolder extends RecyclerView.ViewHolder{
         public TextView userName, userPhoneNumber, userTotalPrice, userDataTime, userShippingAddress;
@@ -89,4 +120,9 @@ public class AdminNewOrdersActivity extends AppCompatActivity {
             showOrderBtn = itemView.findViewById(R.id.show_all_products_btn);
         }
     }
+
+    private void RemoveOrder(String uID) {
+        ordersRef.child(uID).removeValue();
+    }
+
 }
